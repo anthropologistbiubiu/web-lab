c70892c7f5246ea1f28ae9d9958516aa010bdee8
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2026-01-29 17:41:12 +0800

    Add expect function with toBe and notToBe methods
    Implement custom expect function for assertions

--- algorithm/expect.js
+++ algorithm/expect.js
@@ -1,1 +1,29 @@
+var expect = function (val) {
+  let origin = val;
+  return (A = {
+    toBe(arg) {
+      if (arg !== null && arg === origin) {
+        return true;
+      } else {
+        throw "Not Equal";
+      }
+    },
+    notToBe(arg) {
+      if (arg === null || arg !== origin) {
+        return true;
+      } else {
+        throw "Equal";
+      }
+    },
+  });
+};
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+
+console.log(expect(5).notToBe(6));
+
+c = { a: 1, b: "a" };
+console.log(c);
 
