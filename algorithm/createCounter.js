c5f557dc66fcb3ed9a6f286e142d2754a36c9009
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2026-02-05 11:15:53 +0800

    Update createCounter.js

--- algorithm/createCounter.js
+++ algorithm/createCounter.js
@@ -7,8 +7,18 @@ var createCounter1 = function (n) {
   return n++;
 };
 
-var createCounter2 = function (n) {
-  return function () {
-    return n++;
+var createCounter2 = function (init) {
+  var count = init;
+  return {
+    increment: function () {
+      return ++count;
+    },
+    reset: function () {
+      count = init;
+      return count;
+    },
+    decrement: function () {
+      return --count;
+    },
   };
 };
